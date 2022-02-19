8b5a6aa9b02539dd95a56d0148141d6c3b0c411c
tksat <jpgj5745@gmail.com>
2022-02-19 14:38:50 +0900

    日本語入力対応とMainページのベース作成

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -40,6 +40,9 @@ export default function SignIn({setName}) {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(true)
   const [string, setString] = useState('')
+  const [isComposed, setIsComposed] = useState(false)
+
+  console.log(isComposed)
 
   useEffect(e =>{
     setDisabled( string === '' )
@@ -64,12 +67,16 @@ export default function SignIn({setName}) {
             autoFocus
             onChange = {(e)=>setString(e.target.value)}
             onKeyDown={(e) => {
+              if(isComposed) return
+
               if(e.key === 'Enter'){
                 e.preventDefault()
                 setName(e.target.value)
                 setString('')
               }
             }}
+            onCompositionStart={() => setIsComposed(true)}
+            onCompositionEnd={() => setIsComposed(false)}
           />
           <Button
             type="button"
