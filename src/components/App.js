375535642c9ba4aa7a80d229327d2a8ad2252d58
tksat <jpgj5745@gmail.com>
2022-02-19 14:46:30 +0900

    config.jsonを作成してMainページの出し分け設定

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 
 import SignIn from "./SignIn";
 import Main from "./Main"
+import config from "../config.json"
+console.log({config})
 
 export default () => {
   const [name, setName] = useState('');
 
-  if(name === ''){
+  if(config.signInEnabled && name === ''){
     return <SignIn setName={setName} />
   }else{
     return <Main name={name} />
